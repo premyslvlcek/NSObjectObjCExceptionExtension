4e777269fbf529728524f9481611b9a92bfabaee
Premysl Vlcek <premysl.vlcek@strv.com>
2018-04-08 22:16:07 +0200

    Initial commit

---
+++ NSObjectObjCExceptionExtension/NSObjectObjCExceptionExtension.h
@@ -0,0 +1,19 @@
+//
+//  NSObjectObjCExceptionExtension.h
+//  NSObjectObjCExceptionExtension
+//
+//  Created by Premysl Vlcek on 08/04/2018.
+//  Copyright © 2018 STRV. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for NSObjectObjCExceptionExtension.
+FOUNDATION_EXPORT double NSObjectObjCExceptionExtensionVersionNumber;
+
+//! Project version string for NSObjectObjCExceptionExtension.
+FOUNDATION_EXPORT const unsigned char NSObjectObjCExceptionExtensionVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <NSObjectObjCExceptionExtension/PublicHeader.h>
+
+
